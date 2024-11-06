a126fb0c2b1607a1b8f95e89bf47f4459e37b567
Amir Dhahri <amirdhahri01@gmail.com>
2024-11-06 19:59:09 +0100

    upload category image functionality is done

--- src/components/Admin/Categories/AddCategory.js
+++ src/components/Admin/Categories/AddCategory.js
@@ -11,23 +11,21 @@ export default function CategoryToAdd() {
     name: "",
   });
   //Files
-  const [files, setFiles] = useState([]);
+  const [file, setFile] = useState([]);
   const [fileErrors, setFileErrors] = useState([]);
   //file handle change
   const fileHandleChange = (e) => {
-    const newFiles = Array.from(e.target.files);
+    const newFile = e.target.file;
     //Validation
     const newErrs = [];
-    newFiles.forEach((file) => {
-      if (file?.size < 1000000) {
-        newErrs.push(`${file.name} is too large`);
-      }
-      if (!file?.type?.startsWith("image/")) {
-        newErrs.push(`${file?.name} is not an image`);
-      }
-    });
+    if (newFile?.size < 1000000) {
+      newErrs.push(`${newFile.name} is too large`);
+    }
+    if (!newFile?.type?.startsWith("image/")) {
+      newErrs.push(`${newFile?.name} is not an image`);
+    }
     setFileErrors(newErrs);
-    setFiles(newFiles);
+    setFile(newFile);
   };
   //---onChange---
   const handleOnChange = (e) => {
@@ -38,7 +36,7 @@ export default function CategoryToAdd() {
   const handleOnSubmit = (e) => {
     e.preventDefault();
     //dispatch
-    dispatch(createCategoryAction({ name, image: file }));
+    dispatch(createCategoryAction({ name: formData?.name, image: file }));
   };
   return (
     <>
@@ -82,6 +80,51 @@ export default function CategoryToAdd() {
                     name="name"
                     className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                   />
+                </div>
+              </div>
+              {/* upload images */}
+              <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
+                <label className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
+                  Upload Images
+                </label>
+                <div className="mt-1 sm:col-span-2 sm:mt-0">
+                  <div className="flex max-w-lg justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
+                    <div className="space-y-1 text-center">
+                      <svg
+                        className="mx-auto h-12 w-12 text-gray-400"
+                        stroke="currentColor"
+                        fill="none"
+                        viewBox="0 0 48 48"
+                        aria-hidden="true"
+                      >
+                        <path
+                          d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                          strokeWidth={2}
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                      <div className="flex text-sm text-gray-600">
+                        <label
+                          htmlFor="file-upload"
+                          className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
+                        >
+                          <span>Upload files</span>
+                          <input
+                            hidden
+                            name="images"
+                            value={formData.images}
+                            onChange={fileHandleChange}
+                            type="file"
+                            id="file-upload"
+                          />
+                        </label>
+                      </div>
+                      <p className="text-xs text-gray-500">
+                        PNG, JPG, GIF up to 1MB
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </div>
               <div>
