d9c68f332b55906874516059c6be948ae412965e
Amir Dhahri <amirdhahri01@gmail.com>
2024-11-09 20:56:55 +0100

    An update

--- src/components/Admin/Orders/Customers.js
+++ src/components/Admin/Orders/Customers.js
@@ -2,6 +2,8 @@ import { useDispatch, useSelector } from "react-redux";
 import OrdersStats from "./OrdersStatistics";
 import { useEffect } from "react";
 import { fetchOrdersAction } from "../../../redux/slices/orders/ordersSlice";
+import LoadingComponent from "../../LoadingComp/LoadingComponent";
+import NoDataFound from "../../NoDataFound/NoDataFound";
 
 // const people = [
 //   {
@@ -25,97 +27,111 @@ export default function Customers() {
     error,
     orders: customers,
   } = useSelector((state) => state?.orders);
+  //Remove duplicates
+  const uniqueCustomers = customers.filter((item, idx) => {
+    return (
+      customers
+        ?.map((customer) => customer?._id.toString())
+        .indexOf(item?._id.toString()) === idx
+    );
+  });
   return (
     <div className="px-4 sm:px-6 lg:px-8">
       <div className="sm:flex sm:items-center"></div>
 
       <h3 className="text-lg font-medium leading-6 text-gray-900 mt-3">
-        All Customers
+        All Customers [{uniqueCustomers.length}]
       </h3>
       <div className="-mx-4 mt-3  overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:-mx-6 md:mx-0 md:rounded-lg">
-        <table className="min-w-full divide-y divide-gray-300">
-          <thead className="bg-gray-50">
-            <tr>
-              <th
-                scope="col"
-                className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-              >
-                Full Name
-              </th>
-              <th
-                scope="col"
-                className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
-              >
-                Email
-              </th>
-              <th
-                scope="col"
-                className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
-              >
-                Country
-              </th>
-              <th
-                scope="col"
-                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              >
-                City
-              </th>
-              <th
-                scope="col"
-                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              >
-                Phone
-              </th>
+        {loading ? (
+          <LoadingComponent />
+        ) : customers?.length <= 0 ? (
+          <NoDataFound />
+        ) : (
+          <table className="min-w-full divide-y divide-gray-300">
+            <thead className="bg-gray-50">
+              <tr>
+                <th
+                  scope="col"
+                  className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                >
+                  Full Name
+                </th>
+                <th
+                  scope="col"
+                  className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                >
+                  Email
+                </th>
+                <th
+                  scope="col"
+                  className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
+                >
+                  Country
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  City
+                </th>
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  Phone
+                </th>
 
-              <th
-                scope="col"
-                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              >
-                Postal Code
-              </th>
-              {/* <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                <th
+                  scope="col"
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
+                  Postal Code
+                </th>
+                {/* <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
                 <span className="sr-only">Edit</span>
               </th> */}
-            </tr>
-          </thead>
-          <tbody className="divide-y divide-gray-200 bg-white">
-            {customers.map((customer) => {
-              return (
-                <tr key={customer?.user?._id}>
-                  <td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-6">
-                    {customer?.user?.fullname}
-                  </td>
-                  <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">
-                    {customer?.user?.email}
-                  </td>
-                  <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">
-                    {customer?.shippingAddress?.country}
-                  </td>
-                  <td className="px-3 py-4 text-sm text-gray-500">
-                    {customer?.shippingAddress?.city}
-                  </td>
-                  <td className="px-3 py-4 text-sm text-gray-500">
-                    {customer?.shippingAddress?.phone}
-                  </td>
-                  <td className="px-3 py-4 text-sm text-gray-500">
-                    {customer?.shippingAddress?.postalCode}
-                  </td>
-                  <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                    <a
-                      href="#"
-                      className="text-indigo-600 hover:text-indigo-900"
-                    >
-                      Edit
-                      <span className="sr-only">
-                        , {customer?.user?.fullname}
-                      </span>
-                    </a>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-200 bg-white">
+              {customers.map((customer) => {
+                return (
+                  <tr key={customer?.user?._id}>
+                    <td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-6">
+                      {customer?.user?.fullname}
+                    </td>
+                    <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">
+                      {customer?.user?.email}
+                    </td>
+                    <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">
+                      {customer?.shippingAddress?.country}
+                    </td>
+                    <td className="px-3 py-4 text-sm text-gray-500">
+                      {customer?.shippingAddress?.city}
+                    </td>
+                    <td className="px-3 py-4 text-sm text-gray-500">
+                      {customer?.shippingAddress?.phone}
+                    </td>
+                    <td className="px-3 py-4 text-sm text-gray-500">
+                      {customer?.shippingAddress?.postalCode}
+                    </td>
+                    <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                      <a
+                        href="#"
+                        className="text-indigo-600 hover:text-indigo-900"
+                      >
+                        Edit
+                        <span className="sr-only">
+                          , {customer?.user?.fullname}
+                        </span>
+                      </a>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
